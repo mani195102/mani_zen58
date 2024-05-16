21d4f70ec10464da92e3f0a4332864c35449b32a
Manimuthu <manimuthu@isquarebs.com>
2024-05-16 17:31:38 +0530

    changes

--- User-Management/src/UserItem.jsx
+++ User-Management/src/UserItem.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Button, Card, CardActions, CardContent, Grid, Typography } from '@mui/material';
 
 function UserItem({ user, onEdit, onDelete }) {
+  const truncate = (str, n) => {
+    return str.length > n ? str.substr(0, n - 1) + '...' : str;
+  };
+
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card style={{ backgroundColor: '#000000bd' }}>
@@ -25,7 +29,7 @@ function UserItem({ user, onEdit, onDelete }) {
             <strong style={{ color: 'yellow' }}>Company:</strong> {user.company.name}
           </Typography>
           <Typography variant='body2' style={{ width: '100%', display: 'block', paddingBottom: '5px', marginBottom: '5px', color: '#fff', borderBottom: '1px solid #fff' }}>
-            <strong style={{ color: 'yellow' }}>Catch Phrase:</strong> {user.company.catchPhrase}
+            <strong style={{ color: 'yellow' }}>Catch Phrase:</strong> {truncate(user.company.catchPhrase, 30)}
           </Typography>
           <Typography variant='body2' style={{ width: '100%', display: 'block', paddingBottom: '5px', marginBottom: '5px', color: '#fff', borderBottom: '1px solid #fff' }}>
             <strong style={{ color: 'yellow' }}>BS:</strong> {user.company.bs}
